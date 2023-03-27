1ff01c8da6109293c0d2b1aa8b535399d4463393
Harits <haritsdeveloper@gmail.com>
2023-03-27 13:36:42 +0700

    ui finish but not responsive

--- nurhikmah-app/resources/views/home.blade.php
+++ nurhikmah-app/resources/views/home.blade.php
@@ -1,46 +1,263 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Landing Page For to take the exam for prospective new students" />
+    <meta name="author" content="Haritsdev0981" />
+    <title>Daftar SMPIT</title>
 
-                <div class="card-body">
 
-                    
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Nama</th>
-                                <th scope="col">Nama Ortu</th>
-                                <th scope="col">No. Ortu</th>
-                                <th scope="col">Kartu Keluarga</th>
-                                <th scope="col">Akte Kelahiran</th>
-                                <th scope="col">Bukti Pembayaran</th>
-                            </tr>
-                        </thead>
-                        
-                        <tbody>
-                        @foreach ($siswa ?? '' as $data)
-                            <tr>
-                                <th scope="row">{{$data->id}}</th>
-                                <td>{{$data->nama}}</td>
-                                <td>{{$data->namaOrtu}}</td>
-                                <td>{{$data->nomorOrtu}}</td>
-                                <td><img src="{{asset('/storage/images/'.$data->kartuKeluarga)}}" width="100px" ></td>
-                                <td><img src="{{asset('/storage/images/'.$data->akte)}}" width="100px" ></td>
-                                <td><img src="{{asset('/storage/images/'.$data->buktiPembayaran)}}" width="100px" ></td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!--Icon from Bootstap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- JS -->
+    <script src=""></script>
+
+    <!-- swiper js -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+</head>
+
+<body>
+
+    <!-- Navbar -->
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid container-nav">
+            <a class="navbar-brand text-uppercase" href="#">Smpit Nur Hikmah</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav gap-4">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">PPDB</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Tentang Kami</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Kontak</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- end Navbar -->
+
+    <!-- header -->
+
+    <section class="header" id="header">
+        <div class="header-content container-fluid">
+            <div class="row">
+                <div class="col-md-6 text-white">
+                    <p style="font-size: 1.25rem;">Berakhlak | Mandiri | Berprestasi</p>
+                    <p class="visi">Mencetak generasi berakhlakul karimah, mandiri dan berprestasi akademik optimal</p>
+                    <p style="font-size: 1.25rem; margin-bottom: 2rem;">Menjadikan Nur Hikmah sebagai sekolah
+                        percontohan & lembaga dakwah berbasis pendidikan</p>
+                    <a href="#" class="btn-daftar">Daftar Sekarang</a>
+                </div>
+                <div class="col-md-6">
+                    <img src="img/img-content-h.png" alt="img-content" class="img-header">
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+        <svg xmlns="http://www.w3.org/2000/svg" class="svg-header" viewBox="0 0 1440 320">
+            <path fill="#058F42" fill-opacity="1"
+                d="M0,192L80,165.3C160,139,320,85,480,69.3C640,53,800,75,960,122.7C1120,171,1280,245,1360,282.7L1440,320L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z">
+            </path>
+        </svg>
+    </section>
+    <!-- end header -->
+
+    <!-- seleksi -->
+    <section class="section tahapan" id="tahan">
+        <div class="container-nav">
+            <div class="title-content">
+                <p class="title-judul">Tahapan Seleksi</p>
+                <p class="desc-desk">berikut merupakan tahapan yang harus dilalui ketika
+                    mendaftar di SMPIT Nur Hikmah</p>
+            </div>
+            <div class="content-petunjuk">
+                <div class="container-petunjuk ">
+                    <i class="bi bi-person-vcard bi-tt"></i>
+                    <p class="tt-seleksi">Daftar Ujian</p>
+                    <p style="text-transform: capitalize;">calon siswa akan <br> diminta untuk daftar dan <br>
+                        mengirimkan seluruh <br> bukti pembayaran <br> dan biodata </p>
+                </div>
+                <div class="container-petunjuk ">
+                    <i class="bi bi-person-vcard bi-tt"></i>
+                    <p class="tt-seleksi">Ujian Seleksi</p>
+                    <p style="text-transform: capitalize;">calon siswa akan <br> diminta untuk daftar dan <br>
+                        mengirimkan seluruh <br> bukti pembayaran <br> dan biodata </p>
+                </div>
+                <div class="container-petunjuk ">
+                    <i class="bi bi-person-vcard bi-tt"></i>
+                    <p class="tt-seleksi">Pengumuman Penerimaan</p>
+                    <p style="text-transform: capitalize;">calon siswa akan <br> diminta untuk daftar dan <br>
+                        mengirimkan seluruh <br> bukti pembayaran <br> dan biodata </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end seleksi -->
+
+    <!-- testimoni -->
+    <section class="section testimoni">
+        <div class="container-nav">
+            <div class="row">
+                <div class="col-md-5 flex-row">
+                    <P class="title-judul">Pesan dari Guru</P>
+                    <p class="desc-desk fs-5">Pesan dari para Guru untuk calon siswa SMPIT Nur Hikmah </p>
+                </div>
+
+                <div class="col-md-7">
+                    <div class="swiper mySwiper">
+                        <div class="swiper-wrapper">
+
+                            <div class="swiper-slide px-4 pt-4">
+                                <div class="swiper-text fs-5">
+                                    <p>Bagi dunia, kamu mungkin hanya seorang guru, tetapi bagi siswamu, kamu adalah pahlawan</p>
+                                    <i class="bi bi-quote bi-rotate"></i>
+                                </div>
+                                <div class="border"></div>
+                                <div class="swiper-bio d-flex align-items-center mt-3">
+                                    <img src="img/logo-nurhikmah.png" alt="" style="width: 4rem; border-radius: 20px;">
+                                    <div class="flex-row ms-4">
+                                        <p class="m-0 fs-5">Agusriwarman, SE. M.Pd.</p>
+                                        <p class="m-0 desc-desk">Kepala Sekolah Ketiga SMPIT, Sejak 2021</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="swiper-slide px-4 pt-4">
+                                <div class="swiper-text fs-5">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non impedit, voluptates voluptatum autem eius</p>
+                                    <i class="bi bi-quote bi-rotate"></i>
+                                </div>
+                                <div class="border"></div>
+                                <div class="swiper-bio d-flex align-items-center mt-3">
+                                    <img src="img/logo-nurhikmah.png" alt="" style="width: 4rem;">
+                                    <div class="flex-row ms-4">
+                                        <p class="m-0 fs-5">Agusriwarman, SE. M.Pd.</p>
+                                        <p class="m-0 desc-desk">Kepala Sekolah Ketiga SMPIT, Sejak 2021</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="swiper-slide px-4 pt-4">
+                                <div class="swiper-text fs-5">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non impedit, voluptates voluptatum autem eius</p>
+                                    <i class="bi bi-quote bi-rotate"></i>
+                                </div>
+                                <div class="border"></div>
+                                <div class="swiper-bio d-flex align-items-center mt-3">
+                                    <img src="img/logo-nurhikmah.png" alt="" style="width: 4rem;">
+                                    <div class="flex-row ms-4">
+                                        <p class="m-0 fs-5">Agusriwarman, SE. M.Pd.</p>
+                                        <p class="m-0 desc-desk">Kepala Sekolah Ketiga SMPIT, Sejak 2021</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="swiper-slide px-4 pt-4">
+                                <div class="swiper-text fs-5">
+                                    <p>Bagi dunia, kamu mungkin hanya seorang guru, tetapi bagi siswamu, kamu adalah pahlawan</p>
+                                    <i class="bi bi-quote bi-rotate"></i>
+                                </div>
+                                <div class="border"></div>
+                                <div class="swiper-bio d-flex align-items-center mt-3">
+                                    <img src="img/logo-nurhikmah.png" alt="" style="width: 4rem;">
+                                    <div class="flex-row ms-4">
+                                        <p class="m-0 fs-5">Agusriwarman, SE. M.Pd.</p>
+                                        <p class="m-0 desc-desk">Kepala Sekolah Ketiga SMPIT, Sejak 2021</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- <div class="swiper-pagination bg-warning"></div> -->
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- end testimoni -->
+
+    <section class="section footer m-0 p-0">
+        <div class="container-nav d-flex justify-content-center bg-success">
+            <div class="container py-4">
+                <div class="row mt-2 gap-1">
+                    <div class="col-md-4 mt-2">
+                        <img src="img/logo-nurhikmah.png" alt="img-content" class="img-footer">
+                    </div>
+                    <div class="col-md-4 text-white">
+                        <p class="fs-4">Kontak Kami</p>
+                        <table class="table borderless">
+                            <tr>
+                                <td>phone</td>
+                                <td>:</td>
+                                <td>(021) 84599575</td>
+                            </tr>
+                            <tr>
+                                <td>email</td>
+                                <td>:</td>
+                                <td class="text-lowecase">smpitnurhikmah@gmail.com</td>
+                            </tr>
+                        </table>
+                        <div class="sosmed d-flex justify-content-start">
+                            <a href="#" class="icf-links"><i class="bi bi-instagram fs-5"></i></a>
+                            <a href="#" class="icf-links"><i class="bi bi-youtube fs-5"></i></a>
+                            <a href="#" class="icf-links"><i class="bi bi-facebook fs-5"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-3 text-white">
+                        <div class="card-black text-uppercase">
+                            <p class="fs-4">Lokasi Kami</p>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8323.82448792056!2d106.92390107080396!3d-6.308351833826043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69929647524c2d%3A0x166eeed3af2d0a2c!2sSMPIT%20NUR%20HIKMAH!5e0!3m2!1sid!2sid!4v1679891610596!5m2!1sid!2sid" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <script>
+        // Swiper js code
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1.25,
+            spaceBetween: 24,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+
+    </script>
+
+</body>
+
+</html>
+
+
+<!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,192L80,165.3C160,139,320,85,480,69.3C640,53,800,75,960,122.7C1120,171,1280,245,1360,282.7L1440,320L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg> -->
