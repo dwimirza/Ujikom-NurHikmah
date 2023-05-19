71069333c487159d3c1bcf80bfb2413964b9fcb3
Ananda Dwi Mirza <ananda120206@gmail.com>
2023-05-19 22:12:59 +0700

    create table hasil only

---
+++ nurhikmah-app/app/Http/Controllers/HasilController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hasil;
+use Illuminate\Http\Request;
+use App\Models\Exam;
+use App\Models\Question;
+use App\Models\User;
+
+class HasilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $hasil = new Hasil;
+        $user =  User;
+
+            
+        
+    }
+    
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Hasil  $hasil
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hasil $hasil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Hasil  $hasil
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Hasil $hasil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Hasil  $hasil
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hasil $hasil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Hasil  $hasil
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hasil $hasil)
+    {
+        //
+    }
+}
